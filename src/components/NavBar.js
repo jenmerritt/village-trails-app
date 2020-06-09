7906228b44e7e2364a446a86bf18a435f0e20952
Jen Merritt <jenjomez@gmail.com>
2020-06-09 11:02:46 +0100

    navbar desktop complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './styles/NavBar.css';
 
 function NavBar() {
     return (
-      <div className="nav-bar">
-        <p>This is the Nav bar.</p>
+      <div className="nav-bar-wrapper">
+        <div className="nav-bar-left">
+            <h1>Explore Crayke</h1>
+        </div>
+        <div className="nav-bar-center">
+            <img src="../header-image.png" id="header-image" />
+        </div>
+        <div className="nav-bar-right">
+            <p><a href="#">Crayke History Website</a></p>
+        </div>
       </div>
     );
   }
